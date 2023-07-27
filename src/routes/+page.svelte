7aece61a145f8b5d8ca14367ee1ae1fbdadc3862
Navheen Pannirselvam (1000113572) <PANNN2@student.op.ac.nz>
2023-07-28 08:40:24 +1200

    Homework 2.1.1 HTML practice

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,12 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h2>HTML Practice</h2>
+<ul>
+
+<li> cat  </li>
+<li> dog  </li>
+<li> fish </li>
+<li> bird </li>
+<img src="https://2.bp.blogspot.com/-2PItbECvOVs/WKCWkq_aEaI/AAAAAAAACj8/rABnQTv5SFA-8HaFBDWSsiCXxNCKOuUrgCPcB/s1600/Attenborosaurus%2BWitton%2B2017%2Blow%2Bres.jpg">
+
+</ul>
+
+<a href="https://www.op.ac.nz/"> Otago Polytech </a>
