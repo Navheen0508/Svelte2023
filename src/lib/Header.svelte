16c62790f17eb35b481eb4f3072652072c7eb5d6
Navheen Pannirselvam (1000113572) <PANNN2@student.op.ac.nz>
2023-08-01 11:12:31 +1200

    Formatted content into the main div and edited some content on pages

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -1,5 +1,12 @@
+<script>
+        import Nav from"$lib/Nav.svelte";
+</script>
+
+
 <header>
 
     Hello, I am the Header!
+
+    <Nav/>
 
 </header>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,42 +1,35 @@
 <style>
 
-.Div{
-    width:500px;
-    height:500px;
+.div{
+    width:1880px;
+    height:1080px;
     border: 5px solid;
 }
 
 </style>
 
 <script>
-    import Header  from "../lib/Header.svelte";
+    import Header  from "$lib/Header.svelte";
 </script>
 
 <Header/>
-<div class="Div">
+<div class="div">
 
              <h2>HTML Practice</h2>
 
+             <ul>
+
+                <li> cat  </li>
+                <li> dog  </li>
+                <li> fish </li>
+                <li> bird </li>
+            
+            </ul>
+
+            <img src="https://2.bp.blogspot.com/-2PItbECvOVs/WKCWkq_aEaI/AAAAAAAACj8/rABnQTv5SFA-8HaFBDWSsiCXxNCKOuUrgCPcB/s1600/Attenborosaurus%2BWitton%2B2017%2Blow%2Bres.jpg">
+            <a href="https://www.op.ac.nz/"> Otago Polytech </a>
+
 </div>
-
-<nav>
-    <a href="/html/">HTML</a>
-    <a href="/css/">CSS</a>
-    <a href="/js/"> JavaScript</a>
-    <a href="/python/"> Python</a>
-</nav>
-
-<ul>
-
-    <li> cat  </li>
-    <li> dog  </li>
-    <li> fish </li>
-    <li> bird </li>
-
-</ul>
-
-<img src="https://2.bp.blogspot.com/-2PItbECvOVs/WKCWkq_aEaI/AAAAAAAACj8/rABnQTv5SFA-8HaFBDWSsiCXxNCKOuUrgCPcB/s1600/Attenborosaurus%2BWitton%2B2017%2Blow%2Bres.jpg">
-<a href="https://www.op.ac.nz/"> Otago Polytech </a>
 
 <footer>
 
