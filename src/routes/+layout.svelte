a83c5c74d410bd2173cbbade322d604a4bc5f199
Navheen Pannirselvam (1000113572) <PANNN2@student.op.ac.nz>
2023-08-04 08:52:37 +1200

    Added about page and layout files/Homework3.2

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,14 @@
+<p>I am the layout file</p>
+<script>
+    import Header  from "$lib/Header.svelte";
+</script>
+
+<Header/>
+
+<slot/>
+
+<footer>
+
+    <p>Made by Navheen</p>
+
+</footer>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,31 +8,15 @@
 
 </style>
 
-<script>
-    import Header  from "$lib/Header.svelte";
-</script>
 
-<Header/>
 <div class="div">
 
              <h2>HTML Practice</h2>
 
-             <ul>
 
-                <li> cat  </li>
-                <li> dog  </li>
-                <li> fish </li>
-                <li> bird </li>
-            
-            </ul>
 
             <img src="https://2.bp.blogspot.com/-2PItbECvOVs/WKCWkq_aEaI/AAAAAAAACj8/rABnQTv5SFA-8HaFBDWSsiCXxNCKOuUrgCPcB/s1600/Attenborosaurus%2BWitton%2B2017%2Blow%2Bres.jpg">
             <a href="https://www.op.ac.nz/"> Otago Polytech </a>
 
 </div>
 
-<footer>
-
-    <p>Made by Navheen</p>
-
-</footer>
