a6b530c121965cc20a89a2d76674d749cd18903c
Navheen Pannirselvam (1000113572) <PANNN2@student.op.ac.nz>
2023-08-25 09:30:16 +1200

    changed header logo and moving buttons into the index

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,30 +3,28 @@
 
           <div class="mainIndexHeader">
 
-                    <div class="indexHeaderLogo">
-
-
-                    </div>
+                    <img class="containImg" src="../img/msindexlogo.png" alt="gundam icon" height="100%" width="100%">
                     
-                    <div class="mainIndexNav">
-
-                            
-
-                                <nav>
-                                    <ul>
-                                        <li><a href="/html/"><button class="indexNavigationButtons">Story</button></a></li>
-                                        <li><a href="/html/"><button class="indexNavigationButtons">Characters</button></a></li>
-                                        <li><a href="/html/"><button class="indexNavigationButtons">Vistamps</button></a></li>
-                                        <li><a href="/html/"><button class="indexNavigationButtons">Episode Guide</button></a></li>
-                                    </ul>
-                                    
-                                </nav>
-
-                            
-
-                    </div>
 
           </div>
+
+          <div class="mainIndexNav">
+
+                            
+
+            <nav>
+                <ul>
+                    <li><a href="/html/"><button class="indexNavigationButtons">Story</button></a></li>
+                    <li><a href="/html/"><button class="indexNavigationButtons">Characters</button></a></li>
+                    <li><a href="/html/"><button class="indexNavigationButtons">Vistamps</button></a></li>
+                    <li><a href="/html/"><button class="indexNavigationButtons">Episode Guide</button></a></li>
+                </ul>
+                
+            </nav>
+
+        
+
+</div>
     
 </div>
 
