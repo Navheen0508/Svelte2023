90aeae76fd5ef2e10537f0f9ae66e69d138dced2
Navheen Pannirselvam (1000113572) <PANNN2@student.op.ac.nz>
2023-09-01 09:26:34 +1200

    Removed buttons and added a new logo

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,31 +1,17 @@
 
 <div class="mainIndexContainer">
-
-          <div class="mainIndexHeader">
-
-                    <img class="containImg" src="../img/msindexlogo.png" alt="gundam icon" height="100%" width="100%">
-                    
-
-          </div>
-
-          <div class="mainIndexNav">
-
-                            
-
-            <nav>
-                <ul>
-                    <li><a href="/html/"><button class="indexNavigationButtons">Story</button></a></li>
-                    <li><a href="/html/"><button class="indexNavigationButtons">Characters</button></a></li>
-                    <li><a href="/html/"><button class="indexNavigationButtons">Vistamps</button></a></li>
-                    <li><a href="/html/"><button class="indexNavigationButtons">Episode Guide</button></a></li>
-                </ul>
-                
-            </nav>
-
-        
-
-</div>
     
+    <div class="mainIndexHeader">
+        
+        <div class="headerImages">
+            
+            <img class="containImg indexLogoImg" src="../img/indexlogo.png" alt="title icon">
+        
+        </div>
+        
+    
+    </div>
+
 </div>
 
 
