a1936a759306f8ff06ab7ff374b571f9b8a498ca
Navheen Pannirselvam (1000113572) <PANNN2@student.op.ac.nz>
2023-09-01 09:49:05 +1200

    Added new buttons in the mainIndexHeader and wallpaper

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,6 +8,15 @@
             <img class="containImg indexLogoImg" src="../img/indexlogo.png" alt="title icon">
         
         </div>
+
+        <div class ="mainIndexButtonsContainer">
+            
+            <button class="indexNavigationButtons">Button 1</button>
+            <button class="indexNavigationButtons">Button 2</button>
+            <button class="indexNavigationButtons">Button 3</button>
+            <button class="indexNavigationButtons">Button 4</button>
+
+        </div>
         
     
     </div>
