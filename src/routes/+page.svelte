3255c4ef10ebfced0d9cc7d8e2d955ee082dad4b
Navheen Pannirselvam (1000113572) <PANNN2@student.op.ac.nz>
2023-11-02 18:00:31 +1300

    Started creating About page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -23,17 +23,19 @@
         <div class="headerImages">
             
             <img class="containImg indexLogoImg" src="../img/indexlogo.png" alt="title icon">
+
+            <div class ="mainIndexButtonsContainer">
+            
+                <button class="indexNavigationButtons">Mobile Suits</button>
+                <button class="indexNavigationButtons">Titles</button>
+                <button class="indexNavigationButtons">Shows</button>
+                <a href="about"><button class="indexNavigationButtons">About</button></a>
+        
+            </div>
         
         </div>
 
-        <div class ="mainIndexButtonsContainer">
-            
-            <button class="indexNavigationButtons">Mobile Suits</button>
-            <button class="indexNavigationButtons">Titles</button>
-            <button class="indexNavigationButtons">Shows</button>
-            <button class="indexNavigationButtons">About</button>
-
-        </div>
+        
         
     
     </div>
