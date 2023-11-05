893a7063f6e54b4060e0772f023b5d3170988b56
Navheen Pannir <navdx0508@gmail.com>
2023-11-05 18:31:38 +1300

    Added a new video and thumnail and formatted code

--- src/routes/shows/+page.svelte
+++ src/routes/shows/+page.svelte
@@ -31,10 +31,54 @@
         </video>
 
         <ul>
-            <li onclick="videoslider('../video/gun1intro.mp4')"><video src="../video/gun1intro.mp4" class="vSlider" poster="../img/gun1th.JPG" controls> <track kind="captions"> </video></li>
-            <li onclick="videoslider('../video/gun2intro.mp4')"><video src="../video/gun2intro.mp4" class="vSlider" poster="../img/gun2th.JPG" controls> <track kind="captions"> </video></li>
-            <li onclick="videoslider('../video/gun3intro.mp4')"><video src="../video/gun3intro.mp4" class="vSlider" poster="../img/gun3th.JPG" controls> <track kind="captions"> </video></li>
-            <li onclick="videoslider('../video/gun4intro.mp4')"><video src="../video/gun4intro.mp4" class="vSlider" poster="../img/gun4th.JPG" controls> <track kind="captions"> </video></li>
+            <li onclick="videoslider('video1','../video/gun1intro.mp4')">
+
+                <video id="video1" src="../video/gun1intro.mp4" class="vSlider" poster="../img/gun1th.JPG" controls> 
+
+                    <track kind="captions"> 
+
+                </video>
+            
+            </li>
+
+            <li onclick="videoslider('video2','../video/gun2intro.mp4')">
+
+                <video id="video2" src="../video/gun2intro.mp4" class="vSlider" poster="../img/gun2th.JPG" controls> 
+
+                    <track kind="captions"> 
+
+                </video>
+
+            </li>
+
+            <li onclick="videoslider('video3','../video/gun3intro.mp4')">
+
+                <video id="video3" src="../video/gun3intro.mp4" class="vSlider" poster="../img/gun3th.JPG" controls> 
+
+                    <track kind="captions"> 
+
+                </video>
+            </li>
+
+            <li onclick="videoslider('video4','../video/gun4intro.mp4')">
+
+                <video id="video4" src="../video/gun4intro.mp4" class="vSlider" poster="../img/gun4th.JPG" controls> 
+
+                    <track kind="captions"> 
+
+                </video>
+
+            </li>
+
+            <li onclick="videoslider('video5','../video/gun5intro.mp4')">
+
+                <video id="video5" src="../video/gun5intro.mp4" class="vSlider" poster="../img/gun5th.JPG" controls> 
+
+                    <track kind="captions"> 
+
+                </video>
+                
+            </li>
 
 
         </ul>
@@ -51,7 +95,10 @@
 
 
 <script>
-    function videoslider(links){
-        document.querySelector(".vSlider").src = links;
-    }
+    
+    function videoslider(links) {
+    const vSlider = document.querySelector(".vSlider");
+    vSlider.src = links;
+    vSlider.play(); // Autoplay the new video
+}
 </script>
