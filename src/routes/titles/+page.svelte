a2c6d3eb6742b9e0ec3e7f4924b97783afe9e42d
Navheen Pannir <navdx0508@gmail.com>
2023-11-08 23:32:50 +1300

    added main divs and buttons to title page

---
+++ src/routes/titles/+page.svelte
@@ -0,0 +1,23 @@
+<div class="mainIndexContainer">
+
+    <div class="mainIndexHeader">
+        
+        <div class="headerImages">
+            
+            <img class="containImg indexLogoImg" src="../img/indexlogo.png" alt="title icon">
+        
+        </div>
+
+        <div class ="mainIndexButtonsContainer">
+            
+            <a href="/"><button class="indexNavigationButtons">Mobile Suits</button></a>
+            <a href="titles"><button class="indexNavigationButtons">Titles</button></a>
+            <a href="shows"><button class="indexNavigationButtons">Shows</button></a>
+            <a href="about"><button class="indexNavigationButtons">About</button></a>
+            <a href="/"><button class="homeButton"></button></a>
+
+        </div>
+
+    </div>
+
+</div>
