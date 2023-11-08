3e37e9c642def9997c6c691e920447f71ecea8d3
Navheen Pannir <navdx0508@gmail.com>
2023-11-09 11:59:35 +1300

    Embeded youtube videos

--- src/routes/titles/+page.svelte
+++ src/routes/titles/+page.svelte
@@ -25,20 +25,21 @@
         <div>
             
             <h2>Gundam Versus</h2>
-
+            <iframe width="550" height="400"src="https://www.youtube.com/embed/7G2ZnfExRXI" title="test"></iframe>
           
         </div>
 
         <div>
             
             <h2>Gundam VS Maxiboost ON</h2>
+            <iframe width="550" height="400"src="https://www.youtube.com/embed/WBvYIGzos2w" title="test"></iframe>
 
-          
         </div>
 
         <div>
             
             <h2>Gundam Full Extreme VS</h2>
+            <iframe width="550" height="400"src="https://www.youtube.com/embed/5rCLBgB3mnQ" title="test"></iframe>
 
           
         </div>
